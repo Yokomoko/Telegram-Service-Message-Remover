b0950db1d8f5511daf187633223dcc9e40959327
Yokomoko <yokomoko_chapster@live.co.uk>
2018-05-01 14:13:19 +0100

    first commit

---
+++ ServiceMessageRemover/ServiceMessageRemover/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Telegram.Bot;
+using Telegram.Bot.Args;
+using Telegram.Bot.Types.Enums;
+using Telegram.Bot.Types;
+
+namespace ServiceMessageRemover {
+    class Program {
+        public static TelegramBotClient BotClient = new TelegramBotClient(); // Your API Key here
+
+        static void Main() {
+            var me = BotClient.GetMeAsync().Result;
+            Console.Title = me.Username;
+            BotClient.OnMessage += BotOnMessageReceived;
+
+            var arr = new[] { UpdateType.MessageUpdate };
+            BotClient.StartReceiving(arr);
+            Console.ReadLine();
+        }
+
+        private static async void BotOnMessageReceived(object sender, MessageEventArgs messageEventArgs) {
+            if (messageEventArgs.Message.Type == MessageType.ServiceMessage) {
+                try {
+                    await BotClient.DeleteMessageAsync(messageEventArgs.Message.Chat.Id, messageEventArgs.Message.MessageId);
+                    Console.WriteLine("Deleted Service Message");
+                    Console.ReadLine();
+                }
+                //Prevent exception being thrown on previously deleted messages, sometimes they come through multiple times for some reason.
+                catch { }
+            }
+        }
+    }
+}
